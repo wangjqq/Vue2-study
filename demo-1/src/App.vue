e722ff81c75698bd8623ec3d10f081ae681b0c0d
wjq <1092229612@qq.com>
2022-05-14 18:02:04 +0800

    20220514

--- demo-1/src/App.vue
+++ demo-1/src/App.vue
@@ -5,18 +5,36 @@
 
     <div class="box">
       <!-- 渲染 Left 组件和 Right 组件 -->
+      <!-- 3.以标签形式,使用注册好的组件 -->
+      <Left></Left>
+      <Right></Right>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+// 1.导入需要使用的 .vue 组件
+import Left from "@/components/Left.vue";
+import Right from "@/components/Left.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  // 2.注册组件
+  components: {
+    // 'Left':Left,
+    Left,
+    Right,
+  },
+};
 </script>
 
 <style lang="less">
 .app-container {
   padding: 1px 20px 20px;
-  background-color: #efefef;
+  background-color: #4a4a4a;
 }
 .box {
   display: flex;
