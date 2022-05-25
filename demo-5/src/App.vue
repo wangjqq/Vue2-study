33653d82484098989e5d2b10d8c0621c75cbcf45
wjq <1092229612@qq.com>
2022-05-25 11:49:42 +0800

    动态组件

---
+++ demo-5/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app-container">
+    <h1>App 根组件</h1>
+    <hr />
+
+    <div class="box">
+      <!-- 渲染 Left 组件和 Right 组件 -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less">
+.app-container {
+  padding: 1px 20px 20px;
+  background-color: #efefef;
+}
+.box {
+  display: flex;
+}
+</style>
